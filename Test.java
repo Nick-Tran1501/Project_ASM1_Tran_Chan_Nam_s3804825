a9e433dcf33a07ed5201a4f6eec90360791079b8
Nick-Tran1501 <68211831+Nick-Tran1501@users.noreply.github.com>
2022-03-24 11:15:11 +0700

    update

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String args[]) throws FileNotFoundException {
@@ -8,40 +9,46 @@ public class Test {
         Course c2 = new Course("C002", "Further", 12);
         Student s1 = new Student("s001", "minh", "15/01/2001");
         Student s2 = new Student("s002", "ben", "15/01/2001");
-//        List<String> studentEnroll = new ArrayList<>();
-//        List<String> studentCourse = new ArrayList<>();
-        StudentList student1= new StudentList();
-//
-//        if (c1.enroll(s1)) {
-//            System.out.println("Successfully enrolled");
-//
-//        } else {
-//            System.out.println("Already in there");
-//        }
-//        if (c1.enroll(s2)) {
-//            System.out.println("Successfully enrolled");
-//
-//        } else {
-//            System.out.println("Already in there");
-//        }
-//
-//        if (c2.enroll(s1)) {
-//            System.out.println("Successfully enrolled");
-//
-//        } else {
-//            System.out.println("Already in there");
-//        }
+        ArrayList<String> studentEnroll = new ArrayList<>();
+//        ArrayList<String> studentCourse = new ArrayList<>();
 
-//        studentEnroll.add(s1.getCourseList().toString());
+        StudentEnrolment student1= new StudentEnrolment();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please input your StudentID, Name, DOB");
+        Student s3 = new Student(scanner.nextLine(),scanner.nextLine(),scanner.nextLine());
+
+        if (c1.enroll(s3)) {
+            System.out.println("Successfully enrolled");
+
+        } else {
+            System.out.println("Already in there");
+        }
+        if (c1.enroll(s2)) {
+            System.out.println("Successfully enrolled");
+
+        } else {
+            System.out.println("Already in there");
+        }
+
+        if (c2.enroll(s1)) {
+            System.out.println("Successfully enrolled");
+
+        } else {
+            System.out.println("Already in there");
+        }
+
+        studentEnroll.add(s1.getCourseList().toString());
 //        studentCourse.add(c1.getStudentList().toString());
 
-//        System.out.println(c1.getStudentList().toString());
-        student1.addStudent(s1);
-        student1.addStudent(s2);
-        student1.addtofile();
+        System.out.println(c1.getStudentList().toString());
+        System.out.println(s3.getCourseList().toString());
+//        student1.addStudent(s1);
+//        student1.addStudent(s2);
+////        student1.addtofile();
+//
 //        student1.addCourse(c1);
 //        student1.setSemester("2021A");
-        System.out.println(student1);
+//        System.out.println(student1);
 //        System.out.println(s1.toString() + "\n" + studentEnroll);
 //        System.out.println(studentCourse);
     }
